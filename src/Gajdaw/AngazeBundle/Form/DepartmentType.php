d1ef2db9d2fe0625fd6894b907b79090a71edd65
Lukasz Mrugala <mrugala88@gmail.com>
2013-04-05 10:09:25 +0200

    MigrateDepartment

---
+++ src/Gajdaw/AngazeBundle/Form/DepartmentType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Gajdaw\AngazeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DepartmentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Gajdaw\AngazeBundle\Entity\Department'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'gajdaw_angazebundle_departmenttype';
+    }
+}
