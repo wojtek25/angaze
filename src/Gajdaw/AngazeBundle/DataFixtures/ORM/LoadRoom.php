052965841e062e9b8c75f8d4901965da0abcd4d9
Piotr <kosz.piotr@gmail.com>
2013-04-12 09:47:04 +0200

    LoadRoom

---
+++ src/Gajdaw/AngazeBundle/DataFixtures/ORM/LoadRoom.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Gajdaw\AngazeBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Gajdaw\AngazeBundle\Entity\Room;
+use Symfony\Component\Yaml\Yaml;
+
+class LoadRoom implements FixtureInterface
+{
+    function load(ObjectManager $manager)
+    {
+        $filename =
+            __DIR__ .
+                DIRECTORY_SEPARATOR . '..' .
+                DIRECTORY_SEPARATOR . '..' .
+                DIRECTORY_SEPARATOR . 'Data/room.yml';
+
+        $yml = Yaml::parse(file_get_contents($filename));
+        foreach ($yml as $item) {
+            $room = new Room();
+            $room->setName($item['name']);
+            $manager->persist($room);
+        }
+        $manager->flush();
+
+    }
+}
